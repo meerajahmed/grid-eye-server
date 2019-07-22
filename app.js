b1efca503d71eaa92182e8987c2eb3de10d272da
Meeraj Ahmed <meeraj.ahmed@publicissapient.com>
2019-07-22 19:58:52 +0530

    Install socket.io

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const path = require("path");
 const http = require('http');
 const express = require('express');
-const app = express();
+const socketio = require('socket.io');
 
+const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
+
+
 const publicPath = path.join(__dirname, "public");
 const port = 3000;
 
@@ -11,6 +15,13 @@ app.use(express.static(publicPath));
 
 app.get('*',(req, res) => {
     res.sendFile(path.join(publicPath, "index.html"));
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 server.listen(port, () => {

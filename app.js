f8660a7ba3d64938a82a3c3c0ef3b0c4899fff59
Meeraj Ahmed <meeraj.ahmed@publicissapient.com>
2019-07-23 13:02:32 +0530

    Use heroku port

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const io = socketio(server);
 
 
 const publicPath = path.join(__dirname, "public");
-const port = 3000;
+const port = process.env.PORT || 3000; // port set by heroku
 
 app.use(express.static(publicPath));
 

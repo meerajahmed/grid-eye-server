0a2f9e1486170b7a8884bb6b7d8aa7e4e89053a2
Meeraj Ahmed <meeraj.ahmed@publicissapient.com>
2019-07-25 16:01:32 +0530

    Changes for demo

--- app.js
+++ app.js
@@ -24,13 +24,13 @@ io.on('connection', (socket) => {
     // client ref -> socket
     console.log('Grid eye connected');
 
-/*    socket.on('from_grid_eye', function(data) {
+    socket.on('from_grid_eye', function(data) {
        console.log(data);
        socket.broadcast.emit("to_grid_eye_client", data);
-    });*/
+    });
 
     // for testing
-    setTimeout(() => { io.emit('to_grid_eye_client', { count: 1, id: 3 });}, 500);
+/*    setTimeout(() => { io.emit('to_grid_eye_client', { count: 1, id: 3 });}, 500);
     setTimeout(() => { io.emit('to_grid_eye_client', { count: 3, id: 3 });}, 202);
     setTimeout(() => { io.emit('to_grid_eye_client', { count: 1, id: 3 });}, 1000);
     setTimeout(() => { io.emit('to_grid_eye_client', { count: 2, id: 3 });}, 504);
@@ -43,7 +43,7 @@ io.on('connection', (socket) => {
     setTimeout(() => { io.emit('to_grid_eye_client', { count: 3, id: 3 });}, 12006);
     setTimeout(() => { io.emit('to_grid_eye_client', { count: 6, id: 3 });}, 12007);
     setTimeout(() => { io.emit('to_grid_eye_client', { count: 7, id: 3 });}, 12008);
-    setTimeout(() => { io.emit('to_grid_eye_client', { count: 8, id: 3 });}, 12009);
+    setTimeout(() => { io.emit('to_grid_eye_client', { count: 8, id: 3 });}, 12009);*/
 
 
     socket.on('disconnect', () => {
